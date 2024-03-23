3a6279e05ffa767a148ff8bd42095f8c2ce96c90
Pranjal <happybarnwal007@gmail.com>
2024-03-23 12:46:58 +0530

    feat2 added

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ const greet=(name)=>{
 function isAbove18(age){
     console.log(age>=18);
 }
+function isBelow18(age){
+    console.log(age<18);
+}
+
+function newFeatAdded(){
+    console.log("New feat added");
+}
+function newFeat2Added(){
+    console.log("New feat added");
+}
